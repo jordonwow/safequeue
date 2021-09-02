62d8d1025dba3123a7457634c9857ca67b99688e
jordon <29447509+jordonwow@users.noreply.github.com>
2021-09-01 20:42:29 -0700

    Revert "use PVPReadyDialog"
    This reverts commit e287babfc69b592f31ba6ead2c731c0e3e9a8ffc.

--- SafeQueue.lua
+++ SafeQueue.lua
@@ -3,105 +3,135 @@
 
 local addonName, addon = ...
 
+local C_Map = C_Map
 local CreateFrame = CreateFrame
 local DEFAULT_CHAT_FRAME = DEFAULT_CHAT_FRAME
+local ENTER_BATTLE = ENTER_BATTLE
 local GetBattlefieldPortExpiration = GetBattlefieldPortExpiration
 local GetBattlefieldStatus = GetBattlefieldStatus
 local GetBattlefieldTimeWaited = GetBattlefieldTimeWaited
-local GetMapInfo = C_Map.GetMapInfo
 local GetMaxBattlefieldID = GetMaxBattlefieldID
 local GetTime = GetTime
+local InCombatLockdown = InCombatLockdown
+local MiniMapBattlefieldDropDown = MiniMapBattlefieldDropDown
+local PLAYER = PLAYER
 local PVPReadyDialog = PVPReadyDialog
-local PlaySound = PlaySound
-local SOUNDKIT = SOUNDKIT
 local SecondsToTime = SecondsToTime
+local StaticPopup_Visible = StaticPopup_Visible
 local TOOLTIP_UPDATE_TIME = TOOLTIP_UPDATE_TIME
+local UnitInBattleground = UnitInBattleground
 local hooksecurefunc = hooksecurefunc
 
 local SafeQueue = CreateFrame("Frame", "SafeQueue")
 SafeQueue:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
 SafeQueue:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
-SafeQueue:RegisterEvent("ADDON_LOADED")
+SafeQueue.timer = TOOLTIP_UPDATE_TIME
 
-local EXPIRES_FORMAT_RETAIL = "Expires in |cf%s%s|r"
-local EXPIRES_FORMAT_CLASSIC = "\nSafeQueue expires in |cff%s%s|r\n\n|cff%s%s|r"
+local EXPIRES_FORMAT = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE and "Expires in " or "SafeQueue expires in ") ..
+    "|cf%s%s|r"
 local ANNOUNCE_FORMAT = "Queue popped %s"
 
-local ALTERAC_VALLEY = GetMapInfo(1459) and GetMapInfo(1459).name or "Alterac Valley"
-local WARSONG_GULCH = GetMapInfo(1460) and GetMapInfo(1460).name or "Warsong Gulch"
-local ARATHI_BASIN = GetMapInfo(1461) and GetMapInfo(1461).name or "Arathi Basin"
-
-local COLORS = {
-    default = "ffd100",
-    [ALTERAC_VALLEY] = "007fff",
-    [WARSONG_GULCH] = "00ff00",
-    [ARATHI_BASIN] = "ffd100",
-}
-
-function SafeQueue:ADDON_LOADED(name)
-    if name == addonName then
-        self:UnregisterEvent("ADDON_LOADED")
-        self.queues = {}
-        self.timer = TOOLTIP_UPDATE_TIME
-        self.retail = PVPReadyDialog.label
-        self.format = self.retail and EXPIRES_FORMAT_RETAIL or EXPIRES_FORMAT_CLASSIC
-        if (not self.retail) then
-            PVPReadyDialog:SetHeight(120)
-
-            -- add a minimize button
-            local hideButton = CreateFrame("Button", nil, PVPReadyDialog, "UIPanelCloseButton")
-            hideButton:SetNormalTexture("Interface\\Buttons\\UI-Panel-HideButton-Up")
-            hideButton:SetPushedTexture("Interface\\Buttons\\UI-Panel-HideButton-Down")
-            hideButton:SetPoint("TOPRIGHT", PVPReadyDialog, "TOPRIGHT", -3, -3)
-            hideButton:SetScript("OnHide", function() PlaySound(SOUNDKIT.U_CHAT_SCROLL_BUTTON) end)
-        end
-    end
-end
-
-local function GetBattlefieldColor(mapName)
-    return COLORS[mapName] or COLORS.default
-end
-
-function SafeQueue:SetText(battlefieldId)
+local function GetTimerText(battlefieldId)
     if (not battlefieldId) then return end
     local secs = GetBattlefieldPortExpiration(battlefieldId)
     if secs <= 0 then secs = 1 end
     local color
     if secs > 20 then
-        color = "20ff20"
+        color = "f20ff20"
     elseif secs > 10 then
-        color = "ffff00"
+        color = "fffff00"
     else
-        color = "ff0000"
+        color = "fff0000"
     end
-    if self.retail then
-        PVPReadyDialog.label:SetText(EXPIRES_FORMAT_RETAIL:format(color, SecondsToTime(secs)))
-    else
-        local _, mapName = GetBattlefieldStatus(battlefieldId)
-        PVPReadyDialog.text:SetText(EXPIRES_FORMAT_CLASSIC:format(
-            color, SecondsToTime(secs),
-            GetBattlefieldColor(mapName), mapName
-        ))
-    end
+    return EXPIRES_FORMAT:format(color, SecondsToTime(secs))
 end
 
-hooksecurefunc("PVPReadyDialog_Display", function(self, i)
-    self = self or PVPReadyDialog
-    if self.leaveButton then self.leaveButton:Hide() end
-    if self.hideButton then self.hideButton:Hide() end
-    self.enterButton:ClearAllPoints();
-    self.enterButton:SetPoint("BOTTOM", self, "BOTTOM", 0, 25)
-    SafeQueue:SetText(i)
-end)
+if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
+    SafeQueue:SetScript("OnUpdate", function(self, elapsed)
+        if (not PVPReadyDialog:IsShown()) or (not PVPReadyDialog.activeIndex) then return end
+        local timer = self.timer
+        timer = timer - elapsed
+        if timer <= 0 then
+            PVPReadyDialog.label:SetText(GetTimerText(PVPReadyDialog.activeIndex))
+        end
+        self.timer = timer
+    end)
 
-SafeQueue:SetScript("OnUpdate", function(self, elapsed)
-    local battlefieldId = PVPReadyDialog.activeIndex
-    if (not PVPReadyDialog:IsShown()) or (not battlefieldId) then return end
-    local timer = self.timer
-    timer = timer - elapsed
-    if timer <= 0 then self:SetText(battlefieldId) end
-    self.timer = timer
-end)
+    hooksecurefunc("PVPReadyDialog_Display", function(self)
+        self.leaveButton:Hide()
+        self.enterButton:ClearAllPoints();
+        self.enterButton:SetPoint("BOTTOM", self, "BOTTOM", 0, 25)
+    end)
+else
+    local ALTERAC_VALLEY = C_Map.GetMapInfo(1459).name
+    local WARSONG_GULCH = C_Map.GetMapInfo(1460).name
+    local ARATHI_BASIN = C_Map.GetMapInfo(1461).name
+    local COLORS = {
+        [ALTERAC_VALLEY] = { r = 0, g = 0.5, b = 1 },
+        [WARSONG_GULCH] = { r = 0, g = 1, b = 0 },
+        [ARATHI_BASIN] = { r = 1, g = 0.82, b = 0 },
+    }
+    SafeQueue.popup = SafeQueuePopup
+    SafeQueue:RegisterEvent("PLAYER_REGEN_ENABLED")
+
+    function SafeQueue:Create(battlefieldId)
+        if SafeQueue:FindPopup(battlefieldId) then return end
+        local status, battleground = GetBattlefieldStatus(battlefieldId)
+        if status ~= "confirm" then return end
+        if InCombatLockdown() then
+            self.createPopup = battlefieldId
+            return
+        end
+        if StaticPopup_Visible("CONFIRM_BATTLEFIELD_ENTRY") then
+            StaticPopup_Hide("CONFIRM_BATTLEFIELD_ENTRY")
+        end
+        self.createPopup = nil
+        self.popup.hidePopup = nil
+        self.popup.battleground = battleground
+        self.popup.battlefieldId = battlefieldId
+        self.popup.text:SetText(GetTimerText(battlefieldId))
+        self.popup.SubText:SetText(battleground)
+        local color = COLORS[battleground]
+        if color then self.popup.SubText:SetTextColor(color.r, color.g, color.b) end
+        self.popup.EnterButton:SetAttribute("macrotext", "/click MiniMapBattlefieldFrame RightButton\n" ..
+            "/click DropDownList1Button" .. ((battlefieldId * 3) - 1))
+        self.popup:Show()
+    end
+
+    function SafeQueue:FindPopup(battlefieldId)
+        if self.popup:IsVisible() and self.popup.battlefieldId == battlefieldId then
+            return self.popup
+        end
+    end
+
+    function SafeQueue:PLAYER_REGEN_ENABLED()
+        if self.popup.hidePopup then self.popup:Hide() end
+        if self.createPopup then self:Create(self.createPopup) end
+    end
+
+    SafeQueue:SetScript("OnUpdate", function(self, elapsed)
+        local popup = self.popup
+        if (not popup:IsVisible()) then return end
+        local timer = self.timer
+        timer = timer - elapsed
+        if timer <= 0 then
+            if (not popup.battlefieldId) or GetBattlefieldStatus(popup.battlefieldId) ~= "confirm" then
+                popup:Hide()
+                return
+            end
+            popup.text:SetText(GetTimerText(popup.battlefieldId))
+        end
+        self.timer = timer
+    end)
+
+    hooksecurefunc("StaticPopup_Show", function(name, _, _, i)
+        if name ~= "CONFIRM_BATTLEFIELD_ENTRY" then return end
+        if InCombatLockdown() and (not SafeQueue:FindPopup(i)) then return end
+        StaticPopup_Hide(name)
+    end)
+end
+
+SafeQueue.queues = {}
 
 function SafeQueue:Print(message)
     DEFAULT_CHAT_FRAME:AddMessage("|cff33ff99SafeQueue|r: " .. message)
@@ -109,10 +139,11 @@ end
 
 function SafeQueue:UPDATE_BATTLEFIELD_STATUS()
     for i = 1, GetMaxBattlefieldID() do
-        local status, mapName = GetBattlefieldStatus(i)
+        local popup = SafeQueue.FindPopup and SafeQueue:FindPopup(i)
+        local status = GetBattlefieldStatus(i)
         if status == "confirm" then
-            if self.queues[mapName] then
-                local secs = GetTime() - self.queues[mapName]
+            if self.queues[i] then
+                local secs = GetTime() - self.queues[i]
                 local message
                 if secs < 1 then
                     message = "instantly!"
@@ -120,13 +151,18 @@ function SafeQueue:UPDATE_BATTLEFIELD_STATUS()
                     message = "after " .. SecondsToTime(secs)
                 end
                 self:Print(ANNOUNCE_FORMAT:format(message))
-                self.queues[mapName] = nil
+                self.queues[i] = nil
+            end
+            if (not popup) and self.Create then
+                self:Create(i)
             end
         else
-            if mapName and status == "queued" then
-                self.queues[mapName] = self.queues[mapName] or GetTime() - (GetBattlefieldTimeWaited(i) / 1000)
+            if status == "queued" then
+                self.queues[i] = self.queues[i] or GetTime() - (GetBattlefieldTimeWaited(i) / 1000)
             end
-            PVPReadyDialog:Hide()
+            if popup then
+               popup:Hide()
+            end
         end
     end
 end
